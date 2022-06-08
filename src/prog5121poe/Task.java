b099830bc65da6a821f20acbfbfda8e3b739945d
moldybaguette <mfrench4444@gmail.com>
2022-06-08 11:15:20 +0200

    big changes

---
+++ src/prog5121poe/Task.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prog5121poe;
+//way to increment the taskNumber automatically based on the number of existing constructor methods.
+// this is best used for thread saftey when doing multithreading but it works for this purpose too.
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Task
+{
+
+    private static final AtomicInteger aggregator = new AtomicInteger();
+    private static double total = 0;
+    //fields for task object
+    private String taskName;
+    private final int taskNumber;
+    private String taskDescription;
+    private String devDetails;
+    private double taskDuration;
+    private String ID;
+    private String taskStatus;
+
+    public Task()
+    {
+        //this might fuck shit up later
+        taskNumber = aggregator.incrementAndGet();
+    }
+
+    public Task(String taskName, String taskDescript, String devDetails, double taskDuration, String taskStatus)
+    {
+        this.taskName = taskName;
+        this.taskDescription = taskDescript;
+        this.devDetails = devDetails;
+        this.taskDuration = taskDuration;
+        
+        this.taskStatus = taskStatus;
+        this.taskNumber = aggregator.incrementAndGet() -1;
+        createtaskID();
+    }
+
+    public String getTaskStatus()
+    {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus)
+    {
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskName()
+    {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName)
+    {
+        this.taskName = taskName;
+    }
+
+    public int getTaskNumber()
+    {
+        return taskNumber;
+    }
+
+    public String getTaskDescription()
+    {
+        return taskDescription;
+    }
+
+    public String setTaskDescription(String taskDescription)
+    {
+        if (taskDescription.length() <= 50)
+        {
+            this.taskDescription = taskDescription;
+            return "Task successfully captured";
+        } else
+        {
+            return "Please enter a task description of less than 50 characters";
+        }
+    }
+
+    public String getDevDetails()
+    {
+        return devDetails;
+    }
+
+    public void setDevDetails(String devDetails)
+    {
+
+        this.devDetails = devDetails;
+
+    }
+
+    public double getTaskDuration()
+    {
+        return taskDuration;
+    }
+
+    public boolean setTaskDuration(String taskDuration)
+    {
+        try
+        {
+            this.taskDuration = Double.parseDouble(taskDuration);
+        } catch (NumberFormatException ex)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public String getID()
+    {
+        return ID;
+    }
+
+    public boolean checkTaskDescription(String taskDesc)
+    {
+        if (taskDescription.length() <= 50)
+        {
+            this.taskDescription = taskDesc;
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    /*
+    return "Task successfully captured";
+        }
+        else
+            return "Please enter a task description of less than 50 characters"; 
+     */
+    public final void createtaskID()
+    {
+        int nameLength = devDetails.length();
+        String theID = this.taskName.substring(0, 2) + ":" + this.taskNumber + ":" + this.devDetails.substring(nameLength - 3, nameLength);
+        this.ID = theID.toUpperCase();
+    }
+
+    public String printTaskDetails()
+    {
+        return "taskName: " + taskName + "\n"
+                + "taskNumber: " + taskNumber + "\n"
+                + "taskDescription: " + taskDescription + "\n"
+                + "devDetails: " + devDetails + "\n"
+                + "taskDuration: " + taskDuration + "\n"
+                + ", ID: " + ID + "\n"
+                + ", taskStatus: " + taskStatus;
+    }
+     
+    public static double returnTotalHours(double duration)
+    {
+        total += duration;
+        return total;
+    }
+
+}
---
+++ test/prog5121poe/TestTask.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package prog5121poe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mfren
+ */
+public class TestTask
+{
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    public static final Task[] taskArr =
+    {
+        new Task("Create Add Task feature to add task users", "Create Login to authenticate users", "Robyn Harrison", 10, "To Do"),
+        new Task("Create Add Task feature to add task users", "Create Login to authenticate users", "Mike Smith", 12, "Done"),
+        new Task("Create Add Task feature to add task users", "Create Login to authenticate users", "Robyn Harrison", 55, "To Do"),
+        new Task("Create Add Task feature to add task users", "Create Login to authenticate users", "Mike Smith", 11, "Doing"),
+        new Task("Create Add Task feature to add task users", "Create Login to authenticate users", "Robyn Harrison", 1, "To Do")
+    };
+
+    public static final String[] ArrID =
+    {
+        "CR:0:SON",
+        "CR:1:ITH",
+        "CR:2:SON",
+        "CR:3:ITH",
+        "CR:4:SON"
+    };
+
+    @Test
+    public void testCorrectSetTaskDescription()
+    {
+        Task testTask;
+        testTask = new Task("Login Feature", "", "Robyn Harrison", 8, "To Do");
+        String result = testTask.setTaskDescription("Create Login to authenticate users");
+        String expectedResult = "Task successfully captured";
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testIncorrectSetTaskDescription()
+    {
+        Task testTask;
+        testTask = new Task("Login Feature", "", "Robyn Harrison", 8, "To Do");
+        String result = testTask.setTaskDescription("Create Login to authenticate users and some other stuff im not really "
+                + "sure, this is sureley over fifty characters by now? "
+                + "anyyyway ill stop typing now");
+        String expectedResult = "Please enter a task description of less than 50 characters";
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testCorrectCheckTaskDescription()
+    {
+        Task testTask;
+        testTask = new Task("Login Feature", "Create Login to authenticate users", "Robyn Harrison", 8, "To Do");
+        boolean result = testTask.checkTaskDescription(testTask.getTaskDescription());
+        boolean expectedresult = true;
+        assertEquals(expectedresult, result);
+
+    }
+
+    @Test
+    public void testcreatetaskID()
+    {
+        for (int i = 0; i < taskArr.length; i++)
+        {
+            assertEquals(ArrID[i], taskArr[i].getID());
+        }
+    }
+
+    
+
+    @Test
+    public void testReturnTotalHours()
+    {
+        double result = 0;
+        double expectedResult = 89.0;
+        for (int i = 0; i < taskArr.length; i++)
+        {
+            result = Task.returnTotalHours(taskArr[i].getTaskDuration());
+        }
+        assertEquals(expectedResult, result, 0);
+    }
+
+}
